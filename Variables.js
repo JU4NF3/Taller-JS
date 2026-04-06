a1be1ae56ff4e2f55628e8ac0867a7a78f8e3ba8
Juan Felipe Montañez Vanegas <jmontanezv@unbosque.edu.co>
2026-04-06 08:34:49 -0500

    Declaración Variables
    Se declararon las variables de las entradas con sus respectivos valores

---
+++ Variables.js
@@ -0,0 +1,24 @@
+let nombreCompleto = "";
+let edad = 0;
+let tipoDocumento = "";
+let numeroDocumento = "";
+
+let salario = 0;
+let comisiones = 0;
+let totalHorasExtra = 0;
+let nivelRiesgo = "";
+
+const salarioMin = 1750905;
+const salarioMinIntegralVigente = 22761765;
+const subsidioTransporte = 249095;
+const UVT = 50.37;
+
+const porcentajeFondoSolidario = 0.01;
+const porcentajePension = 0.04;
+const porcentajeSalud = 0.04;
+
+const riesgo1 = 0.522;
+const riesgo2 = 1.044;
+const riesgo3 = 2.436;
+const riesgo4 = 4.350;
+const riesgo5 = 6.960;
